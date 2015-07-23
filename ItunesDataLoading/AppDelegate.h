8a53f8b5f92c1f912f68a5bcc9732721104fe789
Yury Voskresensky <yriy.voskresenskiy@arsenalmedia.ru>
2015-07-23 11:24:57 +1000

    first commit

---
+++ ItunesDataLoading/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ItunesDataLoading
+//
+//  Created by Юрий Воскресенский on 23.07.15.
+//  Copyright (c) 2015 Юрий Воскресенский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
